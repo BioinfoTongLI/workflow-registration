f17a0e3d017b55456a33ff0acd1aeea8a75507ab
Tong LI <tl10@sanger.ac.uk>
2021-09-30 16:48:50 +0000

    save all workdirs to NXF_WORK in team283

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@
 # Distributed under terms of the BSD-3 license.
 #
 
-MOUNT_POINT='/lustre/scratch117/cellgen/team283/tl10/NXF_WORK/'
+MOUNT_POINT='/lustre/scratch117/cellgen/team283/NXF_WORK/'
 
 DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
-TRACE_FILE="$MOUNT_POINT/${DATE_WITH_TIME}_registration_trace/registration_trace_${DATE_WITH_TIME}.tsv"
-TMP_NF_WORK="$MOUNT_POINT/${DATE_WITH_TIME}_registration_work"
+TRACE_FILE="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_trace/registration_trace_${DATE_WITH_TIME}.tsv"
+TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_work"
 
 NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK LSB_DEFAULTGROUP='team283' nextflow -trace nextflow.executor run /lustre/scratch117/cellgen/team283/tl10/workflow-registration/main.nf \
 	-params-file $1 \
