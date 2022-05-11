e636fbad2b81d6dbc5b97e194d60751a5b398302
Tong LI <tl10@sanger.ac.uk>
2022-05-11 20:51:43 +0000

    local and hpc run script

---
+++ local_run.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#
+# run.sh
+# Copyright (C) 2021 Tong LI <tongli.bioinfo@gmail.com>
+#
+# Distributed under terms of the BSD-3 license.
+#
+
+MOUNT_POINT='/tmp/work/'
+
+DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
+TRACE_FILE="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_trace/registration_trace_${DATE_WITH_TIME}.tsv"
+TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_work"
+
+NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK LSB_DEFAULTGROUP='team283' nextflow -trace nextflow.executor run main.nf \
+	-params-file $1 \
+	-with-trace $TRACE_FILE \
+	-profile local \
+	-resume
