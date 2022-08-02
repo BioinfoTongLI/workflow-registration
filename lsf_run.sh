48bf4edc24403522089108ae2ad7071701730431
Tong LI <tl10@sanger.ac.uk>
2022-08-02 20:52:35 +0000

    added run scripts

--- lsf_run.sh
+++ lsf_run.sh
@@ -13,8 +13,7 @@ DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
 TRACE_FILE="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_trace/registration_trace_${DATE_WITH_TIME}.tsv"
 TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_work"
 
-NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK LSB_DEFAULTGROUP='team283' /nfs/team283/software/nf/nextflow -trace nextflow.executor run /lustre/scratch117/cellgen/team283/tl10/workflow-registration/main.nf \
+NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK LSB_DEFAULTGROUP='team283' /lustre/scratch117/cellgen/team283/tl10/nextflow/nextflow -trace nextflow.executor run /lustre/scratch117/cellgen/team283/tl10/workflow-registration/main.nf \
 	-params-file $1 \
 	-with-trace $TRACE_FILE \
-	-profile lsf \
-	-resume
+	-profile lsf
