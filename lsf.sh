12faec873ecf627466198ba3b67c0a8f77cdd751
Tong LI <tl10@sanger.ac.uk>
2023-03-18 17:22:01 +0000

    update run scripts

--- lsf.sh
+++ lsf.sh
@@ -6,14 +6,13 @@
 # Distributed under terms of the BSD-3 license.
 #
 
-MOUNT_POINT='/lustre/scratch117/cellgen/team283/NXF_WORK/'
+MOUNT_POINT='/lustre/scratch126/cellgen/team283/NXF_WORK/'
 
 DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
-TRACE_FILE="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_trace/registration_trace_${DATE_WITH_TIME}.tsv"
 TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_work"
 
-NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK LSB_DEFAULTGROUP='team283' /lustre/scratch117/cellgen/team283/tl10/nextflow/nextflow -trace nextflow.executor run /lustre/scratch117/cellgen/team283/tl10/workflow-registration/main.nf \
+NXF_OPTS='-Dleveldb.mmap=false' NXF_VER=22.04.5 NXF_WORK=$TMP_NF_WORK LSB_DEFAULTGROUP='team283' nextflow -trace nextflow.executor run /lustre/scratch126/cellgen/team283/tl10/workflow-registration/main.nf \
 	-params-file $1 \
+	-with-report \
 	-profile lsf
 	#-entry Align_peaks \
-	#-with-trace $TRACE_FILE \
