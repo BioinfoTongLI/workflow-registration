a15c55d11c4e0b12fe1a2ac6d4f184da592b1b50
Tong LI <tl10@sanger.ac.uk>
2023-03-18 22:43:38 +0000

    refact wsireg

--- bin/wsireg_wrapper.py
+++ bin/wsireg_wrapper.py
@@ -13,14 +13,18 @@ import fire
 import numpy as np
 from wsireg.wsireg2d import WsiReg2D
 from wsireg.parameter_maps.reg_params import DEFAULT_REG_PARAM_MAPS
+from ome_types import from_tiff
+from dask_image.imread import imread
 
 
-def main(stem, *images):
+def main(*images, stem="WSIReg", target_ch_index=0):
+    ref_img = sorted(images)[0]
+    ome_md = from_tiff(ref_img, parser="lxml")
+    pixelsize = ome_md.images[0].pixels.physical_size_x
+    print(pixelsize)
+
     # initialize registration graph
     reg_graph = WsiReg2D(stem, stem)
-
-    print(images, type(images))
-
     cycle_names = []
     for i, img in enumerate(images):
         cycle_name = f"cycle{i + 1}"
@@ -29,27 +33,26 @@ def main(stem, *images):
         reg_graph.add_modality(
             cycle_name,
             f"{img}",
-            image_res=0.65,
-            channel_names=["DAPI", "A", "G", "C", "T"],
-            channel_colors=["blue", "green", "red", "yellow", "cyan"],
+            image_res=pixelsize,
+            channel_names=["DAPI", "anchor", "A", "G", "C", "T"],
+            # channel_colors=["blue", "green", "red", "yellow", "cyan"],
             preprocessing={
                 "image_type": "FL",
-                "ch_indices": [1],
+                "ch_indices": [target_ch_index],
                 "as_uint8": True,
                 "contrast_enhance": True,
             },
         )
 
     # reg_graph.add_modality(
-        # "modality_brightfield",
-        # "./data/im2.tiff",
-        # image_res=1,
-        # preprocessing={"image_type": "BF", "as_uint8": True, "invert_intensity": True},
+    # "modality_brightfield",
+    # "./data/im2.tiff",
+    # image_res=1,
+    # preprocessing={"image_type": "BF", "as_uint8": True, "invert_intensity": True},
     # )
 
-
     for cycleName in cycle_names[1:]:
-        print(f"{cycleName} to ref added")
+        print(f"{cycleName} to {cycle_names[0]} added")
         reg_graph.add_reg_path(
             cycleName,
             cycle_names[0],
@@ -59,12 +62,12 @@ def main(stem, *images):
             reg_params=[
                 DEFAULT_REG_PARAM_MAPS["rigid"],
                 DEFAULT_REG_PARAM_MAPS["affine"],
-                DEFAULT_REG_PARAM_MAPS["nl"]
-                ],
+                DEFAULT_REG_PARAM_MAPS["nl"],
+            ],
         )
 
     reg_graph.register_images()
-    reg_graph.save_transformations()
+    # reg_graph.save_transformations()
     reg_graph.transform_images(file_writer="ome.tiff")
 
 
