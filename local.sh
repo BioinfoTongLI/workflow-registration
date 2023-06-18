573e4dbdd5359f1c2c238534a8e0d36b9eb7318a
Tong LI <tl10@sanger.ac.uk>
2023-06-18 22:55:31 +0000

    switch from gitlab version to github

--- local.sh
+++ local.sh
@@ -11,7 +11,7 @@ MOUNT_POINT='/tmp/work/'
 DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
 TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_registration_work"
 
-NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK nextflow -trace nextflow.executor run tl10/workflow-registration \
+NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK nextflow -trace nextflow.executor run BioinfoTongLI/workflow-registration \
 	-params-file $1 \
 	-profile local \
 	-with-report
